d6678f768bef1af62517c235d4b6dc3178ab0a47
Zeyad <zeyadmansour2@gmail.com>
2022-12-29 11:00:40 +0200

    ecg heart rate

--- src/main.py
+++ src/main.py
@@ -5,17 +5,17 @@ from modules.ecg_detection.ecg_main import ecg_analysis
 import pandas as pd
 from scipy.signal import resample
 
-file = 'datasets\X1001.csv'
+file = 'datasets\X1012.csv'
 
 if file.endswith(".csv"):
     fileName = os.path.join(file)
     if os.stat(fileName).st_size != 0:
         rawData = pd.read_csv(fileName, sep=",", header=None, skiprows=1).values
 
-        bcg_data = rawData[:, 5]
+        bcg_data = rawData[:, 2]
         bcg_data = resample(bcg_data, round(50 * len(bcg_data) / 1000))
 
-        ecg_data = rawData[:, 3]
+        ecg_data = rawData[:, 0]
         ecg_data = resample(ecg_data, round(50 * len(ecg_data) / 1000))
 
         bcg_hr = bcg_analysis(bcg_data)
--- src/modules/ecg_detection/ecg_main.py
+++ src/modules/ecg_detection/ecg_main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 import pathlib
@@ -7,14 +8,35 @@ import heartpy as hp
 import os
 import pandas as pd
 from scipy.signal import savgol_filter, resample
+from modules.bcg_detection.modwt_matlab_fft import modwt
+from modules.bcg_detection.modwt_mra_matlab_fft import modwtmra
 
 
 
 def ecg_analysis(data):
 
     sample_rate = 50
-    # wd, m = hp.process(unfiltered_ecg, sample_rate, windowsize=10)
-    wd, m = hp.process_segmentwise(data, sample_rate, 10)
+
+    w = modwt(data, 'bior3.9', 4)
+    dc = modwtmra(w, 'bior3.9')
+    wavelet_cycle = dc[4]
+
+    # limit = int(math.floor(data.size / 500))
+    # t1, t2, window_length, window_shift = 0, 500, 500, 500
+
+    # all_rate = []
+    # for j in range(0, limit):
+    #     sub_signal = wavelet_cycle[t1:t2]
+    #     if j == 23: print(np.mean(sub_signal))
+    #     wd, m = hp.process(sub_signal, sample_rate)
+    #     all_rate.append(round(m['bpm'], 1))
+    #     t1 = t2
+    #     t2 += window_shift
+    # # all_rate = np.vstack(all_rate).flatten()
+
+    # print(all_rate)
+
+    wd, m = hp.process_segmentwise(wavelet_cycle, sample_rate, 10) # overlap of 0.25 removes nan
     print([round(x, 1) for x in m['bpm']])
 
     return [round(x, 1) for x in m['bpm']]
